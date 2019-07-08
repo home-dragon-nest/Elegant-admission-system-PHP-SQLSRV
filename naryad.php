02b3a0fae3f78a959a5310e1e2c29bfe5d3f5b0c
AleksandrSikorskiy <52446856+AleksandrSikorskiy@users.noreply.github.com>
2019-07-08 10:44:22 +0300

    Create naryad.php

---
+++ naryad.php
@@ -0,0 +1,24 @@
+<html>
+
+<script language="javascript">
+        function updateValue(id, value)
+        {
+            // this gets called from the popup window and updates the field with a new value
+            document.getElementById(id).value = value;
+            echo value;
+        }
+
+//        function changeParent() {
+//$idnew=window.opener.document.getElementById('$idn').value;
+//        echo $idnew;
+//        return $idnew;
+
+</script>
+
+<body>
+
+    echo "0808";
+
+</body>
+
+</html>
